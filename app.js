20f134fce4fdb9db25be1a2c28f40751e4c4d6bb
majd hajar <almassalibyaturk@gmail.com>
2022-02-07 15:57:12 +0300

    Create Clothing Store

---
+++ app.js
@@ -0,0 +1,23 @@
+/*--=== NAV JS ===--*/
+const createNav = () => {
+  let nav = document.querySelector("navbar");
+};
+
+createNav();
+
+/*--=== HOME JS ===--*/
+const productContainers = [...document.querySelectorAll(".product-container")];
+const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
+const preBtn = [...document.querySelectorAll(".pre-btn")];
+
+productContainers.forEach((item, i) => {
+  let containerDimenstions = item.getBoundingClientRect();
+  let containerWidth = containerDimenstions.width;
+
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
